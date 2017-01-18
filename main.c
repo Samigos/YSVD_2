5cb7ca276e5df712bfb23deffab2270bccd4a279
Samigos <s.kaniras@gmail.com>
2017-01-18 13:21:17 +0200

    New main

--- main.c
+++ main.c
@@ -1,15 +1,56 @@
-//
-//  main.c
-//  YSVD_2
-//
-//  Created by Sotiris Kaniras on 14/01/2017.
-//  Copyright © 2017 com.me. All rights reserved.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "BF.h"
 #include "Sorted.h"
 
-int main(int argc, const char * argv[]) {
-    Sorted_CreateFile("popp");
-    return 0;
+#define fileName "heapFile"
+
+void insert_Entries(int);
+
+int main(int argc, char **argv) {
+    int fd;
+    int fieldNo;
+   
+    BF_Init();
+    
+    //create heap file
+    if (Sorted_CreateFile(fileName) == -1)
+        printf("Error creating file!\n");
+    
+    fd = Sorted_OpenFile(fileName);
+    
+    if (fd == -1)
+        printf("Error opening file!\n");
+    
+    insert_Entries(fd);
+    
+    //sort heap file using 2-way merge-sort
+    if (Sorted_SortFile(fileName, 0) == -1)
+        printf("Error sorting file!\n");
+    
+    if (Sorted_checkSortedFile("heapFileSorted", 0) == -1)
+        printf("Error sorting file!\n");
+    
+    //get all entries with value
+    //char value[20];
+    //strcpy(value, "Keratsini");
+    
+    fieldNo = 0;
+    int value = 11903588;
+    
+    fd = Sorted_OpenFile("heapFileSorted0");
+    
+    if( fd == -1  )
+        printf("Error opening file!\n");
+    
+    Sorted_GetAllEntries(fd, &fieldNo, &value);
+    
+    return EXIT_SUCCESS;
+}
+
+void insert_Entries(int fd) {
+    //TO DO
+    //read from input stream and write on fd
 }
