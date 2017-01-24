3a4442e8b1091e2c3999fafb88137fa9eb6888f0
Samigos <s.kaniras@gmail.com>
2017-01-24 17:34:12 +0200

    Bla

--- Heap.c
+++ Heap.c
@@ -219,7 +219,7 @@ int HP_SplitFiles(char* initialHeapFileName, const int fieldNo) {
     
     int blockIndex;
     numberOfFiles = numberOfBlocks;
-    
+    printf("\n init numberOfFiles %d\n", numberOfFiles);
     for (blockIndex = 1; blockIndex < numberOfBlocks; blockIndex++) {
         void* block;
         int currentFileDesc;
